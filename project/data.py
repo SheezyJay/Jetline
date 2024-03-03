f88d80934510ec0b5ef73719e44a941b4208fff5
Kanthak <johannes.kanthak@wts.de>
2024-03-03 13:49:30 +0100

    add compiler for exe

---
+++ project/data.py
@@ -0,0 +1,31 @@
+
+from jetline.data.data import Data
+from jetline.data.helper import choose_file
+
+
+class YourDataClass(Data):
+   
+    def __init__(self):
+        super().__init__(
+            name="Name",
+            data=self.get_data(),
+          
+        )
+
+    def get_data(self):
+        file = choose_file()
+        return file
+
+
+class SapData(Data):
+   
+    def __init__(self):
+        super().__init__(
+            name="Sap",
+            data=self.get_data(),
+          
+        )
+
+    def get_data(self):
+        file = choose_file()
+        return file
---
+++ project/pipelines/example_pipeline/nodes.py
@@ -0,0 +1,27 @@
+def node_function_1(data):
+    """
+    An example function that processes data and returns a modified version.
+
+    Args:
+        data: The input data to be processed.
+
+    Returns:
+        str: The processed data.
+    """
+    print("Input Data:", data)
+    processed_data = data + "asasdasdd"
+    print("Processed Data:", processed_data)
+    return processed_data
+
+def node_function_2(data):
+    """
+    An example function that returns the input data unchanged.
+
+    Args:
+        data: The input data.
+
+    Returns:
+        str: The input data.
+    """
+    print("Input Data:", data)
+    return data
