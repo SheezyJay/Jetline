8e54264d3539597b12b4d4357f2ff77d61bb17c6
Joe3009 <112690216+SheezyJay@users.noreply.github.com>
2024-03-16 19:22:17 +0100

    add fast api server routes

--- jetline/visualization/server/server.py
+++ jetline/visualization/server/server.py
@@ -1,12 +1,24 @@
-from flask import Flask
-from flask_webgui import FlaskUI  # Import FlaskUI
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
+from flaskwebgui import FlaskUI
+from typing import Any
 
-app = Flask(__name__, static_folder='dist', static_url_path='')
+app: FastAPI = FastAPI()
 
-@app.route('/')
-def index():
-    return "asd"
+@app.get("/")
+async def index(request: Request) -> FileResponse:
+    """
+    Returns the index.html file.
+    """
+    return FileResponse('dist/index.html')
 
+@app.get("/assets/{file_path:path}")
+async def assets(file_path: str) -> Any:
+    """
+    Returns the requested asset file from the 'dist/assets' directory.
+    """
+    return FileResponse(f"dist/assets/{file_path}")
 
-if __name__ == '__main__':
-   app.run()
+if __name__ == "__main__":
+    # Run the FastAPI application using FlaskUI
+    FlaskUI(app=app, server="fastapi").run()
