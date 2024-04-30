176f1a42420285f5e1dff6fdf6541b12862012f5
johanneskanthak <johannes.kanthak@gmx.de>
2024-04-30 13:51:10 +0200

    update to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jetline',
-    version='0.1.49',
+    version='1.0.0',
     description='Automated Pipeline Builder',
     url='https://github.com/your_username/jetline',
     author='Johannes Kanthak',
